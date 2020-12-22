3450a340dd8784a105c4ea6c3bff17602606d3e7
Boyang Xu <bxu66@inzheng.c.axiomatic-path-288711.internal>
2020-12-22 09:45:53 +0000

    This is second version

--- GlucosePredictionCode.R
+++ GlucosePredictionCode.R
@@ -3,19 +3,19 @@
 #评判值可以通过数据集的不同进行修改
 #OriginalData: 原始数据集
 #返回清理好的数据集
-columnCleaning<-function(OriginalData){
+columnCleaning <- function(OriginalData){
   library(dplyr)
-  dataset<-OriginalData
-  returnSet<-OriginalData
-  number_column<-ncol(dataset)
-  number_row<-nrow(dataset)
-  name<-as.vector(colnames(dataset))
-  for(i in 1:number_column){
-    na<-sum(is.na(dataset[,i]))
-    percetage_missing<-na/number_row
-    if(percetage_missing>0.5){ #评判值
-      dl_variable<-as.character(name[i])
-      returnSet<- returnSet %>% select(-contains(dl_variable))
+  dataset <- OriginalData
+  returnSet <- OriginalData
+  number.column <- ncol(dataset)
+  number.row <- nrow(dataset)
+  name <- as.vector(colnames(dataset))
+  for(i in 1:number.column){
+    na <- sum(is.na(dataset[, i]))
+    percetage.missing <- na / number.row
+    if(percetage.missing > 0.5){ #评判值
+      dl.variable <- as.character(name[i])
+      returnSet <- returnSet %>% select(-contains(dl_variable))
     }
   }
   return(returnSet)
@@ -26,11 +26,16 @@ columnCleaning<-function(OriginalData){
 #return: 返回随机森林模型
 RandomforestModel <- function(dataset){
   library(randomForest)
-  y.index<-length(dataset)
+  y.index <- length(dataset)
   set.seed(1)
-  train.set <- dataset[sample(1:nrow(dataset), nrow(dataset)%/%3*2, replace = FALSE), ]
+  train.set <- dataset[sample(1:nrow(dataset), 
+                       nrow(dataset)%/%3*2, 
+                       replace = FALSE), ]
   test.set <- setdiff(dataset, train.set)
-  model <- randomForest(train.set[, 1:(y.index - 1)], train.set[,y.index], replace=T, keep.forest=T)
+  model <- randomForest(train.set[, 1:(y.index - 1)], 
+                        train.set[,y.index], 
+                        replace=T, 
+                        keep.forest = T)
   return(model)
 }
 
@@ -44,13 +49,13 @@ ImportanceRank <- function(RFModel){
   feature <- rownames(importance)
   feature.importance <- data.frame(feature, importance_1)
   colnames(feature.importance) <- c('feature', 'weights')
-  index.tmp <- order(feature.importance$weights, decreasing = T)
+  index.tmp <- order(feature.importance$weights, 
+                     decreasing = T)
   feature <- as.character(feature.importance$feature)
   feature.importance <- cbind(feature[index.tmp[1:length(feature.importance$weights)]], 
                               feature.importance$weights[index.tmp[1:length(feature.importance$weights)]])
   feature.importance <- data.frame(feature.importance)
   colnames(feature.importance) <- c('feature', 'weights')
-  rm(feature, importance, importance_1, index.tmp)
   return(feature.importance)
 }
 
@@ -78,9 +83,12 @@ GenerateBestData <-function(data, feautre.importance){
     }
     rf.model <- RandomforestModel(data)
     set.seed(1)
-    train.set <- data[sample(1:nrow(data), nrow(data)%/%3*2, replace = FALSE), ]
+    train.set <- data[sample(1:nrow(data), 
+                      nrow(data)%/%3*2, 
+                      replace = FALSE), ]
     test.set <- setdiff(data, train.set)
-    result.preditc <- predict(rf.model, test.set[, 1:n])
+    result.preditc <- predict(rf.model, 
+                              test.set[, 1:n])
     truth<-test.set$葡萄糖
     rmse.total<-rmse.total+sqrt(mean((result.preditc - truth)^2))
     if(n == 192){
@@ -94,7 +102,6 @@ GenerateBestData <-function(data, feautre.importance){
     }
     #如果只剩两个合集则循环结束
     if(n == 2){
-      Sys.time()
       break
     }
   }  
